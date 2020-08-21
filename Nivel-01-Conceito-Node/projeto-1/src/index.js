8e0787a4155963eeb488f8f6fd7b4afea09cb99c
Patrick Cruz <patrickezequielscruz@gmail.com>
2020-08-21 12:09:14 -0300

    Conceito Node

---
+++ Nivel-01-Conceito-Node/projeto-1/src/index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const app = express();
+const { uuid, isUuid } = require('uuidv4');
+
+
+app.use(express.json)
+
+const projects = []
+
+function logRequests(req, res, next) {
+    const { method, url } = request;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    console.log(logLabel);
+
+    return next();
+}
+
+function validateProjectId(req, res, next) {
+    const { id } = request.params;
+
+    if (!isUuid(id)) {
+        return res.status(400).json({ error: 'Invalid project ID.' });
+    }
+
+    return next();
+}
+
+app.use(logRequests)
+
+app.use('/projects/:id', validateProjectId);
+
+app.get('/projects', (req, res) => {
+    const { title } = request.query;
+
+    const results = title
+        ? projects.filter(project => project title.includes(title))
+        : projects;
+
+    console.log('Geted Data')
+    return res.json(project);
+});
+
+app.post('/projects', (req, res) => {
+    const { title, owner } = request.body;
+
+    const project = { id: uuid(), title, owner };
+
+    projects.push(project)
+    return res.json(project);
+});
+
+app.put('/projects/:id', (req, res) => {
+    const { id } = request.params;
+    const { title, owner } = request.body;
+
+    const projectIndex = projects.findIndex(project => project.id == id);
+
+    if (projectIndex < 0) {
+        return res.status(400).json({ error: 'Project not found.' })
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    };
+
+    projects[projectIndex] = project;
+    console.log('Data Changed')
+    return res.json(project);
+});
+
+app.delete('/projects/:id', (req, res) => {
+    const { id } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id == id);
+
+    if (projectIndex < 0) {
+        return res.status(400).json({ error: 'Project not found.' })
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return res.status(204).send();
+});
+
+app.listen(3030, () => {
+    console.log('🦖 Back-end rodando bonitão!')
+})
