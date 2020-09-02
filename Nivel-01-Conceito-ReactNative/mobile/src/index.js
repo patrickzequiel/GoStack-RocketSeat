c21a92b3112e7f3cc11e6b5c09e3c4ff1cd622eb
Patrick Cruz <patrickezequielscruz@gmail.com>
2020-09-01 23:58:20 -0300

    Atualizando, consumindo API com FlatList e SafeAreaView

--- Nivel-01-Conceito-ReactNative/mobile/src/index.js
+++ Nivel-01-Conceito-ReactNative/mobile/src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, StyleSheet, StatusBar } from 'react-native'
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native'
 
 import api from './services/api';
 
@@ -15,11 +15,22 @@ export default function App() {
     return(
         <>
         <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-     <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
+            
+        <FlatList
+        data={repositories}
+        keyExtractor={repository => repository.id}
+        renderItem={({ item: repository}) => (
+            <Text style={styles.project}>{repository.url}</Text>
+        )}
+
+        />
+        </SafeAreaView>
+     {/* <View style={styles.container}>
          {repositories.map(repository => (
-         <Text style={styles.project} key={repository.id}>{repository.url}</Text>
+         
          ))}
-    </View>
+    </View> */}
     </>
     )
 }
@@ -28,8 +39,6 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#7159c1',
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
     },
     project: {
         color: '#FFF',
