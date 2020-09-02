87dd946a770c0cc969914b7ab03dffdbe60d1e3f
Patrick Cruz <patrickezequielscruz@gmail.com>
2020-09-02 00:17:00 -0300

    Atualizando para adicionar repositório quando clicado

--- Nivel-01-Conceito-ReactNative/mobile/src/index.js
+++ Nivel-01-Conceito-ReactNative/mobile/src/index.js
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from 'react'
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native'
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native'
 
 import api from './services/api';
 
 export default function App() {
     const [repositories, setRepositories] = useState([]);
 
+    
     useEffect(() => {
         api.get('repositories').then(response => {
             setRepositories(response.data);
         })
     }, []);
 
+    async function handleAddRepository() {
+        const response = await api.post('repositories', {
+          title: 'Novo projeto',
+          url: `Novo Repositório ${Date.now()}`,
+          techs: ['NodeJs', 'ReactJs']
+        });
+    
+        setRepositories([ ...repositories, response.data ]);
+      }
+
     return(
         <>
         <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
         <SafeAreaView style={styles.container}>
-            
+
         <FlatList
         data={repositories}
         keyExtractor={repository => repository.id}
@@ -25,6 +36,14 @@ export default function App() {
         )}
 
         />
+
+        <TouchableOpacity 
+        activeOpacity={0.5}
+        style={styles.button}
+        onPress={handleAddRepository}
+        >
+            <Text style={styles.buttonText}>Adicionar Repositório</Text>
+        </TouchableOpacity>
         </SafeAreaView>
      {/* <View style={styles.container}>
          {repositories.map(repository => (
@@ -40,9 +59,26 @@ const styles = StyleSheet.create({
         backgroundColor: '#7159c1',
         flex: 1,
     },
+
     project: {
         color: '#FFF',
-        fontSize: 120,
+        fontSize: 20,
         fontWeight: 'bold',
+        margin: 20,
+        padding: 30,
     },
+
+    button: {
+        backgroundColor: '#FFF',
+        margin: 20,
+        height: 50,
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    }
 })
