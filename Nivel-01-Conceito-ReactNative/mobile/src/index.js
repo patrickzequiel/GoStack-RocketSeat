e38acc845dda94f835366bb7b1497a1631a40e7e
Patrick Cruz <patrickezequielscruz@gmail.com>
2020-09-01 23:50:32 -0300

    App consumindo a API do ConceitoNode

--- Nivel-01-Conceito-ReactNative/mobile/src/index.js
+++ Nivel-01-Conceito-ReactNative/mobile/src/index.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet, StatusBar } from 'react-native'
 
+import api from './services/api';
+
 export default function App() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        api.get('repositories').then(response => {
+            setRepositories(response.data);
+        })
+    }, []);
+
     return(
         <>
         <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
      <View style={styles.container}>
-         <Text style={styles.title}>Patrick Cruz</Text>
+         {repositories.map(repository => (
+         <Text style={styles.project} key={repository.id}>{repository.url}</Text>
+         ))}
     </View>
     </>
     )
@@ -19,9 +31,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-    title: {
+    project: {
         color: '#FFF',
-        fontSize: 20,
+        fontSize: 120,
         fontWeight: 'bold',
     },
 })
