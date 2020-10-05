12359057fb8e0a144cd32fb083d159ac5e5a2f99
Patrick Cruz <patrickezequielscruz@gmail.com>
2020-10-04 21:35:02 -0300

    Subindo repositórios

---
+++ Nivel-02/Criando-Repositórios/src/routes/appointments.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { startOfHour, parseISO,  } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentRouter = Router();
+
+
+const appointmentsRepository = new AppointmentsRepository()
+
+appointmentRouter.post('/', (request, response) => {
+    const { provider, date } = request.body;
+
+    const parseDate = startOfHour(parseISO(date));
+
+    const findAppointmentInSameDate = appointmentsRepository.findByDate(parseDate);
+
+    if (findAppointmentInSameDate) {
+        return response.status(400).json({ message: "This appointment is already booked" });
+    }
+
+    const appointment = appointmentsRepository.create(provider, parseDate);
+
+    return response.json(appointment)
+});
+
+export default appointmentRouter;
