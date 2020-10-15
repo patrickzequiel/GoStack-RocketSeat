dc598c10909f5d8abfabe86312fc465c4e6b51ea
Patrick Cruz <patrickezequielscruz@gmail.com>
2020-10-14 21:36:23 -0300

    Tratativa de exceções

--- Nivel-02/Back-End-Aplicação/Upload de Imagens/03-Servindo-Arquivos-Estáticos/src/routes/users.routes.ts
+++ Nivel-02/Back-End-Aplicação/Upload de Imagens/03-Servindo-Arquivos-Estáticos/src/routes/users.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import multer from 'multer'
+import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
@@ -11,59 +11,57 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-    try {
-        const { name, email, password } = request.body;
+  try {
+    const { name, email, password } = request.body;
 
-        const createUser = new CreateUserService();
+    const createUser = new CreateUserService();
 
-        const user = await createUser.execute({
-            name,
-            email,
-            password
-        });
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
 
-        const userWithoutPassword = {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            created_at: user.created_at,
-            updated_at: user.updated_at,
-        }
+    // Com a atualização do TypeScript, isso se faz necessário
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
 
-        return response.json(userWithoutPassword);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
+    return response.json(userWithoutPassword);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 usersRouter.patch(
-    '/avatar',
-    ensureAuthenticated,
-    upload.single('avatar'),
-    async (request, response) => {
-        console.log(request.file);
-        try {
-            const updateUserAvatar = new UpdateUserAvatarService();
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
 
-            const user = await updateUserAvatar.execute({
-                user_id: request.user.id,
-                avatarFilename: request.file.filename,
-            });
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
 
-            const userWithoutPassword = {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                created_at: user.created_at,
-                updated_at: user.updated_at,
-            }
+      const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
 
-            return response.json(userWithoutPassword);
-        } catch (err) {
-            return response.status(400).json({ error: err.message})
-        }
-    });
-
-
-
+      return response.json(userWithoutPassword);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 export default usersRouter;
--- Nivel-02/Back-End-Aplicação/Upload de Imagens/03-Servindo-Arquivos-Estáticos/src/services/UpdateUserAvatarService.ts
+++ Nivel-02/Back-End-Aplicação/Upload de Imagens/03-Servindo-Arquivos-Estáticos/src/services/UpdateUserAvatarService.ts
@@ -5,37 +5,36 @@ import fs from 'fs';
 import uploadConfig from '../config/upload';
 import User from '../models/User';
 
-
-interface Request{
-    user_id: string,
-    avatarFilename: string,
+interface Request {
+  user_id: string;
+  avatarFilename: string;
 }
 
 class UpdateUserAvatarService {
-    public async execute({ user_id, avatarFilename }: Request): Promise<User> {
-      const usersRepository = getRepository(User);
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
 
-      const user = await usersRepository.findOne(user_id);
+    const user = await usersRepository.findOne(user_id);
 
-      if (!user) {
-        throw new Error('Only authenticated user can change avatar');
-      }
-
-      if (user.avatar) {
-        const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-        const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-        if (userAvatarFileExists) {
-          await fs.promises.unlink(userAvatarFilePath);
-        }
-      }
-
-      user.avatar = avatarFilename;
-
-      await usersRepository.save(user);
-
-      return user;
+    if (!user) {
+      throw new Error('Only authenticated user can change avatar');
     }
-  }
 
-  export default UpdateUserAvatarService;
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
